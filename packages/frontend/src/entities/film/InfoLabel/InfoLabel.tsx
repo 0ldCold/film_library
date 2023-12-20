010a06f3dec0eb651968505abdc32de8c0361238
Susskiy <i.susskiy@trueengineering.ru>
2023-12-20 22:45:33 +0700

    Рефакторинг инфо модуля

---
+++ packages/frontend/src/entities/film/InfoLabel/InfoLabel.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from "react";
+import Styles from "./InfoLabel.module.scss";
+
+interface InfoLabelProps {
+  label: string;
+  children?: ReactNode | string;
+}
+const InfoLabel: FC<InfoLabelProps> = ({ label, children }) => {
+  return (
+    <div className={Styles.wrapper}>
+      <span className={Styles.label}>{`${label}: `}</span>
+      <div className={Styles.value}>{children}</div>
+    </div>
+  );
+};
+export default InfoLabel;
---
+++ packages/frontend/src/widgets/film/InfoModule/helpers.ts
@@ -0,0 +1,8 @@
+export const nextEpisodeDateTransform = (date: Date): string => {
+  return date.toLocaleDateString("ru", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+};
