7aae2b43e70242c90ffb379afed610639eca23e7
Evgenia <88364102+Red-Fox-98@users.noreply.github.com>
2024-03-29 14:40:54 +0300

    #23 UI: Фильм. У друзей
    * Добавлен блок на страницу фильма У друзей
    
    * Исправлены функция отображения статуса фильма у друга и стили
    
    * Файл type перенесен в другую папку
    
    * Подправлены стили и функция отображения статуса фильма
    
    * Подправлены стили и функция отображения статуса фильма

---
+++ packages/frontend/src/features/film/Friend/Friend.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { IFilmFriends } from "src/widgets/film/FriendsFilm/types";
+import Styles from "./Friend.module.scss";
+import Avatar404 from "src/entities/film/Author/404Avatar.png";
+import Image from "next/image";
+
+export interface FriendProps {
+  friend: IFilmFriends["friend"];
+}
+
+const Friend: FC<FriendProps> = ({ friend }) => {
+  return (
+    <div className={Styles.friend}>
+      <Image
+        src={friend.avatar ?? Avatar404.src}
+        alt={"аватарка пользователя"}
+        width={16}
+        height={16}
+      />
+      <div className={Styles.name}>
+        <a href=''>{friend.name}</a>
+      </div>
+    </div>
+  );
+};
+
+export default Friend;
---
+++ packages/frontend/src/widgets/film/FriendsFilm/FriendsFilm.tsx
@@ -0,0 +1,27 @@
+import ModuleCard from "src/entities/film/ModuleCard/ModuleCard";
+import Styles from "./FriendsFilm.module.scss";
+import { IFilmFriends } from "src/widgets/film/FriendsFilm/types";
+import Friend from "src/features/film/Friend/Friend";
+import { viewingStatus } from "src/widgets/film/FriendsFilm/helpers";
+import { FC } from "react";
+
+interface FilmFriendsProps {
+  data: IFilmFriends[];
+}
+
+const FriendsFilm: FC<FilmFriendsProps> = ({ data }) => {
+  return (
+    <ModuleCard title={"У друзей"}>
+      <div>
+        {data.map((friendsFilm) => (
+          <div key={friendsFilm.friend.name} className={Styles.friend}>
+            <Friend friend={friendsFilm.friend} />
+            <div className={Styles.status}>{viewingStatus(friendsFilm.list, friendsFilm.rate)}</div>
+          </div>
+        ))}
+      </div>
+    </ModuleCard>
+  );
+};
+
+export default FriendsFilm;
