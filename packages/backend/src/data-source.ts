ca025e2df81fb1a3ade83c3f6c3a732acf7100f2
Susskiy <i.susskiy@trueengineering.ru>
2024-04-12 17:11:58 +0700

    Правки запуска проекта, подключены env файлы

--- packages/backend/src/data-source.ts
+++ packages/backend/src/data-source.ts
@@ -1,17 +1,39 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User.js"
+import {DataSource} from "typeorm"
+import {User} from "./entity/User"
+import type {PostgresConnectionOptions} from "typeorm/driver/postgres/PostgresConnectionOptions";
+import {isExist} from "./helpers/isExist";
+
+type ValidDbConfig = Pick<PostgresConnectionOptions, 'host' | 'port' | 'username' | 'password' | 'database'>;
+type DbConfigFormEnv = Record<keyof ValidDbConfig, string | undefined>;
+const checkValidConfig = (envConfig: DbConfigFormEnv): envConfig is Record<keyof ValidDbConfig, string> => {
+  return isExist(envConfig) &&
+    isExist(envConfig.host) &&
+    isExist(envConfig.port) &&
+    !Number.isNaN(+envConfig.port) &&
+    isExist(envConfig.username) &&
+    isExist(envConfig.password) &&
+    isExist(envConfig.database)
+}
+
+const dbConfig: DbConfigFormEnv = {
+  host: process.env.VITE_DB_HOST,
+  port: process.env.VITE_DN_PORT,
+  username: process.env.VITE_DB_USERNAME,
+  password: process.env.VITE_DB_PASSWORD,
+  database: process.env.VITE_DB_NAME,
+}
+if (!checkValidConfig(dbConfig)) {
+  throw new Error("Проверьте содержимое .env файла. " +
+    "Если его нету - создайте с содержимым из .env.example и отредактируйте");
+}
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+  type: "postgres",
+  ...dbConfig,
+  port: +dbConfig.port,
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
 })
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,52 +1,60 @@
+import "reflect-metadata"
+
 import express from "express"
 import bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source.js"
-import { Routes } from "./routes.js"
-import { User } from "./entity/User.js"
+import {Request, Response} from "express"
+import {AppDataSource} from "./data-source"
+import {Routes} from "./routes"
+import {User} from "./entity/User"
+import {isExist} from "./helpers/isExist";
+
+const port = process.env.VITE_APP_PORT;
+if (!isExist(port) || Number.isNaN(+port)) {
+  throw new Error("Проверьте наличие и правильность порта в .env")
+}
 
 AppDataSource.initialize().then(async () => {
 
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
+  // create express app
+  const app = express()
+  app.use(bodyParser.json())
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+  // register express routes from defined application routes
+  Routes.forEach(route => {
+    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+      const result = (new (route.controller as any))[route.action](req, res, next)
+      if (result instanceof Promise) {
+        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
+      } else if (result !== null && result !== undefined) {
+        res.json(result)
+      }
     })
+  })
 
-    // setup express app here
-    // ...
+  // setup express app here
+  // ...
 
-    // start express server
-    app.listen(3000)
+  // start express server
+  app.listen(+port)
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
+  // insert new users for test
+  await AppDataSource.manager.save(
+    AppDataSource.manager.create(User, {
+      firstName: "Timber",
+      lastName: "Saw",
+      age: 27
+    })
+  )
 
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
+  await AppDataSource.manager.save(
+    AppDataSource.manager.create(User, {
+      firstName: "Phantom",
+      lastName: "Assassin",
+      age: 24
+    })
+  )
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+  console.log(`Express server has started on port 3000. Open http://localhost:${port} to see results`)
 
 }).catch(error => console.log(error))
