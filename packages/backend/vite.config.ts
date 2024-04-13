362cc4312f9446d83b871758be05fc3b055bcc57
Susskiy <i.susskiy@trueengineering.ru>
2024-04-14 02:07:03 +0700

    Фикс запуска

--- packages/backend/vite.config.ts
+++ packages/backend/vite.config.ts
@@ -1,14 +1,12 @@
 import swc from "@rollup/plugin-swc";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig, loadEnv } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
 
 export default defineConfig((data) => {
   const localEnv = loadEnv(data.mode, process.cwd());
 
   return {
-    plugins: [typescript(), swc(), tsconfigPaths()],
+    plugins: [typescript(), swc()],
     define: {
       'process.env': {
         ...process.env,
