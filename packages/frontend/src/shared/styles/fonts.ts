0f57cacecc4bb3cd3baa0f3f8a96c36f06c21fa7
Aeronnax <aeronnaxog@gmail.com>
2024-04-29 20:47:49 +0500

    Фиксы шрифтов

--- packages/frontend/src/shared/styles/fonts.ts
+++ packages/frontend/src/shared/styles/fonts.ts
@@ -4,83 +4,150 @@ export const openSans = localFont({
   variable: "--font-openSans",
   src: [
     {
-      path: "../../../public/fonts/OpenSans/OpenSans-VariableFont_wdth,wght.ttf",
+      path: "../../../public/fonts/OpenSans/OpenSans-Light.ttf",
+      weight: "300",
       style: "normal"
     },
     {
-      path: "../../../public/fonts/OpenSans/OpenSans-Italic-VariableFont_wdth,wght.ttf",
+      path: "../../../public/fonts/OpenSans/OpenSans-LightItalic.ttf",
+      weight: "300",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-Regular.ttf",
+      weight: "400",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-Italic.ttf",
+      weight: "400",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-Medium.ttf",
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-MediumItalic.ttf",
+      weight: "500",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-Bold.ttf",
+      weight: "700",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-BoldItalic.ttf",
+      weight: "700",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/OpenSans/OpenSans-ExtraBoldItalic.ttf",
+      weight: "800",
       style: "italic"
     }
   ]
 });
 
-export const helveticaNeueCyr = localFont({
-  variable: "--font-helveticaNeueCyr",
+export const helveticaNeue = localFont({
+  variable: "--font-helveticaNeue",
   src: [
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Black.ttf",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueThin.otf",
+      weight: "100",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueThinItalic.otf",
+      weight: "100",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueUltraLight.otf",
+      weight: "200",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueUltraLightItalic.otf",
+      weight: "200",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueLight.otf",
+      weight: "300",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueLightItalic.otf",
+      weight: "300",
+      style: "italic"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueRoman.otf",
       weight: "400",
       style: "normal"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-BlackItalic.ttf",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueItalic.ttf",
       weight: "400",
       style: "italic"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Bold.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueMedium.otf",
+      weight: "500",
       style: "normal"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-BoldItalic.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueMediumItalic.otf",
+      weight: "500",
       style: "italic"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Heavy.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueBlack.otf",
+      weight: "600",
       style: "normal"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-HeavyItalic.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueBlackItalic.otf",
+      weight: "600",
       style: "italic"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Italic.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueBold.otf",
+      weight: "700",
+      style: "normal"
+    },
+    {
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueBoldItalic.otf",
+      weight: "700",
       style: "italic"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Light.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueHeavy.otf",
+      weight: "900",
       style: "normal"
     },
     {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-LightItalic.ttf",
-      weight: "400",
+      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueHeavyItalic.otf",
+      weight: "900",
       style: "italic"
-    },
-    {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Medium.ttf",
-      weight: "400",
-      style: "normal"
-    },
-    {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-MediumItalic.ttf",
-      weight: "400",
-      style: "italic"
-    },
-    {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Roman.ttf",
-      weight: "400",
-      style: "normal"
-    },
-    {
-      path: "../../../public/fonts/HelveticaNeueCyr/HelveticaNeueCyr-Thin.ttf",
-      weight: "400",
-      style: "normal"
     }
   ]
 });
--- packages/frontend/src/widgets/template/MainTemplate/MainTemplate.tsx
+++ packages/frontend/src/widgets/template/MainTemplate/MainTemplate.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import SectionWrap from "src/entities/template/SectionWrap/SectionWrap";
-import { helveticaNeueCyr, openSans } from "@styles/fonts";
+import { helveticaNeue, openSans } from "@styles/fonts";
 import { clsx } from "clsx";
 import Styles from "./MainTemplate.module.scss";
 
@@ -11,7 +11,7 @@ interface MainTemplateProps {
 }
 const MainTemplate: FC<MainTemplateProps> = ({ children }) => {
   return (
-    <div className={clsx(openSans.variable, helveticaNeueCyr.variable, Styles.font)}>
+    <div className={clsx(openSans.variable, helveticaNeue.variable, Styles.font)}>
       <Header />
       <main>
         <SectionWrap>{children}</SectionWrap>
