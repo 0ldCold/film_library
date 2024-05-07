89001e9abcb7fe42797a83ed25bb3ed3034a981b
Aeronnax <aeronnaxog@gmail.com>
2024-05-08 01:10:01 +0500

    Фиксы

--- packages/frontend/src/widgets/film/PeoplesListsModule/PeoplesListsModule.tsx
+++ packages/frontend/src/widgets/film/PeoplesListsModule/PeoplesListsModule.tsx
@@ -1,19 +1,22 @@
-import { FC,useMemo } from "react";
+import { FC, useMemo } from "react";
 import { BarChartRowData } from "src/entities/film/BarChartRow/types";
 import ModuleCard from "src/entities/film/ModuleCard/ModuleCard";
 import PeoplesLists from "src/entities/film/PeoplesLists/PeoplesLists";
 import { PeoplesListsInfo } from "src/shared/api/film/types";
-
 interface PeoplesListsModuleProps {
-  data:PeoplesListsInfo[]
+  data: PeoplesListsInfo[];
 }
 
-const PeoplesListsModule: FC<PeoplesListsModuleProps> = ({data}) => {
+const PeoplesListsModule: FC<PeoplesListsModuleProps> = ({ data }) => {
   const transformedData = useMemo<BarChartRowData[]>(
     () => data.map((item) => ({ value: item.count, label: String(item.status) })),
     [data]
   );
-  return <ModuleCard title='В списках у людей'> <PeoplesLists data={transformedData}/></ModuleCard>;
+  return (
+    <ModuleCard title='В списках у людей'>
+      <PeoplesLists data={transformedData} />
+    </ModuleCard>
+  );
 };
 
 export default PeoplesListsModule;
