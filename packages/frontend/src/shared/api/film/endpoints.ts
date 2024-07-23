5430e00f4939ea9c87d82b6c24c197990e76767f
Susskiy <i.susskiy@trueengineering.ru>
2024-07-23 22:56:20 +0700

    Подключение тестового эндпоинта

---
+++ packages/frontend/src/shared/api/film/endpoints.ts
@@ -0,0 +1,6 @@
+import { BackendResponseWrapper } from "src/shared/api/types";
+import { instance } from "src/shared/api/instance";
+
+export const getUsers = (): Promise<BackendResponseWrapper<Components.Schemas.UsersDTO[]>> => {
+  return instance.get("users");
+};
