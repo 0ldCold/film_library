9e86f3d3b11250904065b718574828cfbd39a57b
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2024-08-14 18:14:49 +0300

    Добавлено изменение цвета надписи автора описания

--- packages/frontend/src/features/film/Contributor/Contributor.tsx
+++ packages/frontend/src/features/film/Contributor/Contributor.tsx
@@ -4,16 +4,21 @@ import Image from 'next/image';
 import Avatar404 from 'src/entities/film/Author/404Avatar.png';
 import { FilmDescriptionData } from '../../../widgets/film/DescriptionModule/types';
 import { useTranslation } from 'react-i18next';
+import { useAppSelector } from 'src/shared/store/hooks';
+import { clsx } from 'clsx';
 
 interface ContributorProps {
   data: FilmDescriptionData['contributor'];
 }
 
 const Contributor: FC<ContributorProps> = ({ data }) => {
+  const theme = useAppSelector((state) => state.theme.theme);
   const { t } = useTranslation();
   return (
     <div className={Styles.contributor}>
-      <div className={Styles.type}>{`${t(`film.contributor.${data.type}`)}: `}</div>
+      <div
+        className={clsx(Styles.type, theme === 'light' && Styles.lightType)}
+      >{`${t(`film.contributor.${data.type}`)}: `}</div>
       {data.type !== 'externalSource' && (
         <Image
           className={Styles.img}
